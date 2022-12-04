2060dbd269e5411c2004903977464cfc79775169
SeungyoupBaek <bsy8220@naver.com>
2022-12-05 00:27:40 +0900

    feat: Add Post delete API

--- src/main/java/com/youp/sns/controller/PostController.java
+++ src/main/java/com/youp/sns/controller/PostController.java
@@ -8,6 +8,7 @@ import com.youp.sns.model.Post;
 import com.youp.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,6 +32,12 @@ public class PostController {
     public Response<PostResponse> modify(@PathVariable Integer postId, @RequestBody PostModifyRequest request, Authentication authentication) {
         Post post = postService.modify(request.getTitle(), request.getBody(), authentication.getName(), postId);
         return Response.success(PostResponse.fromPost(post));
+    }
+
+    @DeleteMapping("/{postId}")
+    public Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
+        postService.delete(authentication.getName(), postId);
+        return Response.success();
     }
 
 }
